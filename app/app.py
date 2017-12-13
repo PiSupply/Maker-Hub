01ea8908e14cf3138c48cdaa63e30ca97e0da125
Denis Khrutsky <denis.xpy@gmail.com>
2017-12-13 20:12:01 +0600

    Add signals for install process

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 import sys
 import json
+from multiprocessing import Process, Queue
+from installer import install_package
 from PyQt5.QtWidgets import (QWidget, QApplication, QHBoxLayout, QTextEdit, QListWidgetItem,
                              QVBoxLayout, QLabel, QPushButton, QListView, QListView, QListWidget)
 from PyQt5.QtGui import QStandardItemModel, QStandardItem, QIcon, QPixmap
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal, QObject, QTimer
 
 PACKAGES_FILE = 'resources/packages.json'
+DESTINATION_FOLDER = '/opt'
 
 
 def get_software_objects():
@@ -15,6 +18,11 @@ def get_software_objects():
     return packages
 
 
+class UICommunication(QObject):
+    installBeginSignal = pyqtSignal()
+    installEndSignal = pyqtSignal()
+
+
 class MainWidget(QWidget):
     def __init__(self):
         super().__init__()
@@ -22,6 +30,8 @@ class MainWidget(QWidget):
         self.setGeometry(300, 300, 500, 500)
         self.setMinimumWidth(400)
         self.setMinimumHeight(200)
+
+        self.UISignals = UICommunication()
 
         hbox = QHBoxLayout()
 
@@ -35,7 +45,7 @@ class MainWidget(QWidget):
 
         hbox.addWidget(softwareListWidget)
 
-        vbox = QVBoxLayout()
+        rightVBox = QVBoxLayout()
         titleBox = QHBoxLayout()
         self.itemLabel = QLabel(self)
         self.itemIconLabel = QLabel(self)
@@ -53,21 +63,25 @@ class MainWidget(QWidget):
         titleBox.addWidget(self.itemIconLabel)
         titleBox.addWidget(self.itemLabel)
         titleBox.addLayout(linksBox)
-        vbox.addLayout(titleBox)
+        rightVBox.addLayout(titleBox)
 
         self.descriptionText = QTextEdit()
         self.descriptionText.setReadOnly(True)
-        vbox.addWidget(self.descriptionText)
+        rightVBox.addWidget(self.descriptionText)
 
         self.installBtn = QPushButton("Install", self)
+        self.installBtn.clicked.connect(self.install)
+        self.UISignals.installBeginSignal.connect(self.disableUI)
+        self.UISignals.installEndSignal.connect(self.enableUI)
+
         self.pinoutBtn = QPushButton("Pinout", self)
         btnBox = QHBoxLayout()
         btnBox.addWidget(self.pinoutBtn)
         btnBox.addWidget(self.installBtn)
         # btnBox.addStretch(1)
-        vbox.addLayout(btnBox)
+        rightVBox.addLayout(btnBox)
 
-        hbox.addLayout(vbox)
+        hbox.addLayout(rightVBox)
         self.setLayout(hbox)
 
         self.show()
@@ -75,16 +89,37 @@ class MainWidget(QWidget):
     def onItemChanged(self, curr, prev):
         self.currentProduct = self.softwareObjects[curr.text()]
         self.descriptionText.setText(self.currentProduct['description_full'])
-        self.itemLabel.setText(self.currentProduct['name'])
+        self.itemLabel.setText(self.currentProduct['title'])
         self.itemIconLabel.setPixmap(QPixmap(self.currentProduct['icon_32x32']))
         self.githubLinkLabel.setText(f"<a href='{self.currentProduct['github_link']}'>Github</a>")
         self.storeLinkLabel.setText(f"<a href='{self.currentProduct['website_link']}'>PiSupply</a>")
 
+    def install(self, event):
+        self.UISignals.installBeginSignal.emit()
+        queue = Queue()
+        install_process = Process(target=install_package, args=[self.currentProduct, queue, DESTINATION_FOLDER])
+        install_process.start()
+        install_process.join()
+        for _ in range(queue.qsize()):
+            print(queue.get())
+        QTimer.singleShot(1000, lambda: self.UISignals.installEndSignal.emit())
+        # self.UISignals.installEndSignal.emit()
+
     def _populateSoftwareList(self, listWidget):
         for name, softDict in self.softwareObjects.items():
             item = QListWidgetItem(listWidget)
-            item.setText(name)
+            item.setText(softDict['title'])
             item.setIcon(QIcon(softDict['icon_16x16']))
+
+    def enableUI(self):
+        self.installBtn.setText("Install")
+        self.installBtn.setDisabled(False)
+        print("Enable UI")
+
+    def disableUI(self):
+        self.installBtn.setText("Installing...")
+        self.installBtn.setDisabled(True)
+        print("Disable UI")
 
 
 if __name__ == '__main__':
